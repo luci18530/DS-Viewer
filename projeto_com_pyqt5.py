0e98c09bc357f5e9ff9fbf6493dc8216e21f2d72
JovemPedr0 <plvm0220@gmail.com>
2022-11-28 20:18:16 -0300

    organizando algumas coisas

---
+++ projeto_com_pyqt5.py
@@ -0,0 +1,9 @@
+# A program to create visualizations of the data structures (linked lists, sequential lists, stacks, queues, binary search trees) 
+# PROJETO DE ESTRUTURA DE DADOS
+# Grupo: Guilherme Nogueira, Luciano Pereira, Pedro Lucas, Thais Melquiades, Vitoria Grisi
+from utils import *
+
+textoparainput = ''
+application = PyQt5.QtWidgets.QApplication(sys.argv)
+janela = janelamain()
+sys.exit(application.exec_())
---
+++ utils.py
@@ -0,0 +1,296 @@
+import sys
+import os
+
+import PyQt5
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.Qt import *
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 165, 0)
+PURPLE = (128, 0, 128)
+
+# LISTA ENCADEADA | LINKED LIST (USING PYTHON BUILD-IN LIST)
+class LinkedList:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+    def add(self, item):
+        self.items.append(item)
+
+    def remove(self, item):
+        self.items.remove(item)
+
+    def search(self, item):
+        return item in self.items
+
+    def index(self, item):
+        return self.items.index(item)
+
+    def pop(self, pos):
+        return self.items.pop(pos)
+
+    def insert(self, pos, item):
+        self.items.insert(pos, item)
+
+    def __str__(self):
+        return str(self.items)
+    
+    def __repr__(self):
+        return str(self.items)
+    
+    
+# FILAS | QUEUES
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    def __str__(self):
+        return str(self.items)
+    
+    def __repr__(self):
+        return str(self.items)
+
+# PILHAS | STACKS
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+    def __str__(self):
+        return str(self.items)
+    
+    def __repr__(self):
+        return str(self.items)
+
+# ARVORE BINARIA DE BUSCA | BINARY SEARCH TREE
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    def findval(self, lkpval):
+        if lkpval < self.data:
+            if self.left is None:
+                return str(lkpval)+" Not Found"
+            return self.left.findval(lkpval)
+        elif lkpval > self.data:
+            if self.right is None:
+                return str(lkpval)+" Not Found"
+            return self.right.findval(lkpval)
+        else:
+            return str(self.data) + ' is found'
+
+    
+
+    # PRINT TREE WITH LINES
+    def print_tree(self, level=0):
+        if self.right:
+            self.right.print_tree(level+1)
+            print('\t' * level,'/', sep='')
+        print('\t' * level, self.data)
+        if self.left:
+            print('\t' * level,'\\', sep='')
+            self.left.print_tree(level+1)
+
+    def __str__(self):
+        return str(self.data)
+    
+    def __repr__(self):
+        return str(self.data)
+
+
+liste = LinkedList()
+
+class janelalista(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setStyleSheet("background-color: black;")
+        labellista = QLabel(self)
+        labellista.setText("Lista Encadeada")
+        labellista.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        labellista.move(10, 10)
+        
+        
+        self.inputlista()
+        self.initUI()
+        
+    def initUI(self):
+        self.setGeometry(10, 30, 1050, 700)
+        self.setWindowTitle('Lista Encadeada')
+        self.show()
+    
+    
+
+    def inputlista(self):
+        blalala = 1
+        # user input
+        entrada = QLineEdit(self)
+        entrada.move(10, 40)
+        entrada.resize(200, 20)
+        # set text color to white
+        entrada.setStyleSheet("color: white;")
+        # set placeholder text
+        entrada.setPlaceholderText("Digite um valor")
+        # set text changed event
+        entrada.textChanged.connect(self.onchanged)
+        
+
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Insert:
+            
+            print("Insert")
+            textoparainput = self.findChild(QLineEdit).text()
+            # add to list
+            liste.add(textoparainput)
+            print(liste)
+
+    def test_method(self):
+        print('Space key pressed')
+
+        
+
+    def addlist(self):
+        # add value to linked list
+        liste.add(self.text)
+        # clear text
+        
+
+
+    def onchanged(self, text):
+        print(text)
+        
+        blablabla = 1
+
+        
+class janelamain(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        # set background color to black
+        self.setStyleSheet("background-color: black;")
+        self.left = 10
+        self.top = 30
+        self.width = 1050
+        self.height = 700
+        self.title = 'Estrutura de Dados'
+        exit_button = QPushButton('SAIR', self)
+        exit_button.move(1000, 660)
+        exit_button.resize(50, 40)
+        exit_button.setStyleSheet("background-color: red; color: white; font-weight: bold; font-size: 15px; font-family: Lucida Sans Unicode")
+        exit_button.clicked.connect(self.close)
+        self.label1()
+        self.lista()
+        self.pilha()
+        self.fila()
+        self.arvore()      
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.show()
+
+    def label1(self):
+        label1 = QLabel('Visualizador de Estrutura de Dados', self)
+        # move to the center top of the window
+        label1.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
+        label1.resize(600, 40)
+        label1.setStyleSheet("background-color: black; color: white; font-weight: bold; font-size: 32px; font-family: Lucida Sans Unicode; font-style: italic")
+        label1.show()
+
+    def lista(self):
+        lista_button = QPushButton('Lista Encadeada', self)
+        lista_button.move(10, 60)
+        lista_button.resize(200, 40)
+        lista_button.setStyleSheet("background-color: blue; color: white; font-weight: bold; font-size: 15px; font-family: Lucida Sans Unicode")
+        lista_button.clicked.connect(self.listaclick)
+
+    def listaclick(self):
+        print('Lista Encadeada')
+        self.lista = janelalista()
+  
+
+    def pilha(self):
+        pilha_button = QPushButton('Pilha', self)
+        pilha_button.move(10, 110)
+        pilha_button.resize(200, 40)
+        pilha_button.setStyleSheet("background-color: blue; color: white; font-weight: bold; font-size: 15px; font-family: Lucida Sans Unicode")
+        pilha_button.clicked.connect(self.pilhaclick)
+    
+    def pilhaclick(self):
+        print('Pilha')
+
+    def fila(self):
+        fila_button = QPushButton('Fila', self)
+        fila_button.move(10, 160)
+        fila_button.resize(200, 40)
+        fila_button.setStyleSheet("background-color: blue; color: white; font-weight: bold; font-size: 15px; font-family: Lucida Sans Unicode")
+        fila_button.clicked.connect(self.filaclick)
+
+    def filaclick(self):
+        print('Fila')
+
+    def arvore(self):
+        arvore_button = QPushButton('Arvore', self)
+        arvore_button.move(10, 210)
+        arvore_button.resize(200, 40)
+        arvore_button.setStyleSheet("background-color: blue; color: white; font-weight: bold; font-size: 15px; font-family: Lucida Sans Unicode")
+        arvore_button.clicked.connect(self.arvoreclick)
+    
+    def arvoreclick(self):
+        print('Arvore')
